8c9f8195b5971e416c9310b0f5d134d32b826282
Calvenn Tsuu <calvenn.tsuu@gmail.com>
2017-04-10 15:30:32 -0600

    Add files via upload

---
+++ src/tools.h
@@ -0,0 +1,33 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+class Tools {
+public:
+  /**
+  * Constructor.
+  */
+  Tools();
+
+  /**
+  * Destructor.
+  */
+  virtual ~Tools();
+
+  /**
+  * A helper method to calculate RMSE.
+  */
+  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
+  //simplify the code
+  //VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+  
+
+
+};
+
+#endif /* TOOLS_H_ */
